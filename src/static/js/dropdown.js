74085c371d1af9c8e3d52120a8504c8059e7f34d
Dan Hiester <hiesterd@bloomington.in.gov>
2016-04-27 12:37:29 -0400

    Add classes back to elements inside dropdowns.

--- src/static/js/dropdown.js
+++ src/static/js/dropdown.js
@@ -1,16 +1,15 @@
 (function () {
     "use strict";
     var closeMenus = function () {
-        var openLaunchers = document.querySelectorAll('.dropdown button:first-child[aria-expanded="true"]'),
+        var openLaunchers = document.querySelectorAll('.dropdown [aria-expanded="true"]'),
             len = openLaunchers.length,
             i   = 0;
         for (i=0; i<len; i++) {
             openLaunchers[i].setAttribute("aria-expanded", "");
             openLaunchers[i].parentElement.focus();
-            openLaunchers[i].parentElement.style.zIndex = '10';
             openLaunchers[i].setAttribute("aria-expanded", "false");
             (function (i) {
-                setTimeout(function() { openLaunchers[i].parentElement.querySelector('.dropdown button:first-child + div').style.display = 'none'; }, 300);
+                setTimeout(function() { openLaunchers[i].parentElement.querySelector('.dropdown .links').style.display = 'none'; }, 300);
             })(i);
         }
         document.removeEventListener('click', closeMenus);
@@ -18,11 +17,10 @@
     launcherClick = function(e) {
         var launcher  = e.target,
             container = launcher.parentElement,
-            menu      = launcher.parentElement.querySelector('.dropdown button:first-child + div');
+            menu      = launcher.parentElement.querySelector('.dropdown .links');
         launcher.blur();
         closeMenus();
         menu.style.display = 'block';
-        container.style.zIndex = '20';
         setTimeout(function() {
             launcher.setAttribute("aria-expanded", "true");
         }, 50);
@@ -31,13 +29,11 @@
         e.preventDefault();
         menu.focus();
     },
-    launchers = document.querySelectorAll('.dropdown button:first-child'),
+    launchers = document.querySelectorAll('.dropdown .launcher'),
     len   = launchers.length,
     i = 0;
 
     for (i=0; i<len; i++) {
         launchers[i].addEventListener('click', launcherClick);
-        launchers[i].parentElement.querySelector('.dropdown button:first-child + div').style.display = 'none';
-        launchers[i].parentElement.style.zIndex = '10';
     }
 })();
